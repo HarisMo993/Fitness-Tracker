ef00d791fbb2aaaff977277469c5ae57d60b07f4
haris1993 <harisagic@hotmail.com>
2021-04-18 02:27:30 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCZDyoT4LkTFdcBdRCxdPYsTAeNu1VccpI",
+    authDomain: "ng-fitness-tracker-62277.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-62277-default-rtdb.firebaseio.com",
+    projectId: "ng-fitness-tracker-62277",
+    storageBucket: "ng-fitness-tracker-62277.appspot.com",
+    messagingSenderId: "1048617245552",
+  }
+};
